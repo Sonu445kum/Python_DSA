293296759f64ac92c41af2f9cd4b77337cbad803
Sonu445Kum <sonuroy1629@gmail.com>
2025-08-21 10:25:04 +0530

    problems Solve of Map Functrions

---
+++ 10_Functions/04_Map_Func.py
@@ -0,0 +1,38 @@
+
+# string to integer
+s =['1','2','3','4','5']
+result=map(int,s)
+print(list(result))
+
+# Converting Map object into list
+a =[2,4,6,7,8]
+
+def double(val):
+    return val*2
+
+res =list(map(double,a))
+print(res)
+
+# map with lambda function
+m=[2,3,4,5,6]
+ans =map(lambda x :x*2,m)
+print(list(ans))
+
+# map with multiple Expression
+a1 =[1,2,3,4]
+b1 =[5,6,7,8]
+ans1 =map(lambda x,y:x+y,a1,b1)
+print(list(ans1))
+
+# Converting String to upper case
+s1 =["hello","guys","how","are","you"]
+UpperString =list(map(lambda x:x.upper(),s1))
+print(UpperString)
+
+# Extracting First Character from the String
+s2 =["apple","mango","banana"]
+extractStr=list(map(lambda x:x[0],s2))
+print(extractStr)
+
+# Removing whitespaces from strings
+s3 =['  hello   ' , '   ']
